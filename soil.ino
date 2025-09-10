0922b4c6586bf92d25f5f5d161567815bb895a96
Parimala Chakot <160464715+ParimalaChakot2004@users.noreply.github.com>
2025-09-10 15:21:27 +0530

    Add files via upload

---
+++ soil.ino
@@ -0,0 +1,29 @@
+int sensor=0;
+const int upper=1000;
+const int lower=600;
+
+void setup(){
+  pinMode(A0,INPUT);
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+void(loop)(){
+  sensor=analogRead(A0);
+  
+  
+  if(sensor>=upper){
+      Serial.println(sensor);
+      digitalWrite(13,HIGH);
+  }
+  else if (sensor<upper && sensor>lower){
+      Serial.println(sensor);
+  
+  }
+  else{
+      Serial.println(sensor);
+    digitalWrite(13,LOW);
+  }
+  delay(1000);
+
+}
+
